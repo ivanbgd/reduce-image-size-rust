2efce3e63fc11e377fe1f5bb97745ecf2e61027a
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-12-08 18:26:04 +0100

    Start WIP

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+//! The program's entry point.
+
+use clap::Parser;
+
+// use reduce_image_size::logic::main_loop;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+}
