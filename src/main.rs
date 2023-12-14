297116facbc2fae2baf8935232ff3b6bcae27516
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-12-14 22:31:28 +0100

    Add timing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 
 use clap::Parser;
 use reduce_image_size::cli::Args;
-// use reduce_image_size::logic::process_images;
+use reduce_image_size::logic::process_images;
+use std::time::Instant;
 
 /// The program's entry point.
 fn main() {
+    let start = Instant::now();
+
     let args = Args::parse();
     let src_dir = args.src_dir;
     let dst_dir = args.dst_dir;
@@ -13,9 +16,14 @@ fn main() {
     let resize = args.resize;
     let quality = args.quality;
 
+    // /// REMOVE
     println!(
         "{}, {}, {recursive}, {resize}, {quality}",
         src_dir.display(),
         dst_dir.display()
     );
+
+    process_images();
+
+    println!("\nTook {:.3?} to complete.", start.elapsed());
 }
